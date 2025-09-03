0bd10c554e7968bb22573057f945c271e4924edc
molaei.ma <molaei.ma@SO-IT-PC5>
2025-09-03 17:20:39 +0330

    productForAdmin

---
+++ Store_Example.Application/Services/Products/Queries/GetAllProductForAdmin/IGetAllProductForAdmin.cs
@@ -0,0 +1,96 @@
+﻿using Store_Example.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Store_Example.Application.Interfaces.Contexts;
+using Store_Example.Common;
+using Store_Example.Common.Dtos;
+
+namespace Store_Example.Application.Services.Products.Queries.GetAllProductForAdmin
+{
+	public interface IGetAllProductForAdmin
+	{
+		public Task<ResultDto<ResultGetAllProductForAdminDto>> Execute();
+	}
+
+	public class GetAllProductForAdmin : IGetAllProductForAdmin
+	{
+		private readonly IDatabaseContext _context;
+
+		public GetAllProductForAdmin(IDatabaseContext context)
+		{
+			_context = context;
+		}
+		public async Task<ResultDto<ResultGetAllProductForAdminDto>> Execute(int pageSize, int currentPage)
+		{
+
+			try
+			{
+				int rowCount;
+				var ProductForAdmins = await _context.Products
+														 .Include(x => x.Category)
+														 .Select(p => new ProductForAdminDto()
+														 {
+															 Brand = p.Brand,
+															 Category = p.Category.Name,
+															 Description = p.Description,
+															 Displayed = p.Displayed,
+															 Name = p.Name,
+															 Inventory = p.Inventory,
+															 Price = p.Price
+														 })
+														 .ToPagedAsync(currentPage, pageSize, out rowCount);
+				return new ResultDto<ResultGetAllProductForAdminDto>()
+				{
+					Data = new ResultGetAllProductForAdminDto()
+					{
+						CurrentPage = currentPage,
+						PageSize = pageSize,
+						RowCount = rowCount,
+						ProductForAdminDtos = ProductForAdmins.ToList()
+					},
+					IsSuccess = true,
+					Message = "موفقیت آمیز"
+				};
+			}
+			catch (Exception ex)
+			{
+
+				return new ResultDto<ResultGetAllProductForAdminDto>()
+				{
+					Data = new ResultGetAllProductForAdminDto(),
+					IsSuccess = false,
+					Message = "شکست"
+				};
+			}
+
+
+		}
+	}
+
+	public class ResultGetAllProductForAdminDto
+	{
+		public int RowCount { get; set; }
+		public int CurrentPage { get; set; }
+		public int PageSize { get; set; }
+
+		public ICollection<ProductForAdminDto> ProductForAdminDtos { get; set; }
+	}
+
+	public class ProductForAdminDto
+	{
+
+
+		public string Name { get; set; }
+
+		public string Brand { get; set; }
+		public string Description { get; set; }
+		public int Price { get; set; }
+		public int Inventory { get; set; }
+		public bool Displayed { get; set; }
+		public string Category { get; set; }
+	}
+}
--- Store_Example.Common/PageInation.cs
+++ Store_Example.Common/PageInation.cs
@@ -8,7 +8,7 @@ namespace Store_Example.Common
 {
 	public static class PageInation
 	{ 
-		public static IEnumerable<TSource> ToPaged<TSource>(this IEnumerable< TSource> source, int page,int pageSize,out int rowsCont)
+		public static async Task<IEnumerable<TSource>> ToPagedAsync<TSource>(this IEnumerable< TSource> source, int page,int pageSize,out int rowsCont)
 		{
 			rowsCont=source.Count();
 			return source.Skip((page -1)* pageSize).Take(pageSize);
